c4841ae5e896a240cb15131bbd6c12a0f72b26a7
Luka Mazmishvili <lukka.mazmishvili@gmail.com>
2023-05-02 21:24:55 +0400

    Added Functionality, Implemented ViewModel for configuration changes. Code needs improvement.

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.kt
@@ -1,23 +1,47 @@
 package com.example.mycalculatorapp
 
-//import androidx.activity.viewModels
+import androidx.activity.viewModels
 import android.app.UiModeManager
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.mycalculatorapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
-//    private val viewModel: MainViewModel by viewModels()
+    private var operation = ""
+    private var n1 = 0.0
+    private var n2 = 0.0
+
+
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN
+//        )
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
         setContentView(binding.root)
 
 //        themeSwitchView()
@@ -32,6 +56,59 @@ class MainActivity : AppCompatActivity() {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
 
+        listeners()
+
+    }
+
+    private fun listeners() {
+        with(binding) {
+            tvAC.setOnClickListener(this@MainActivity)
+            tvDivision.setOnClickListener(this@MainActivity)
+            ivPlusMinus.setOnClickListener(this@MainActivity)
+            tvEqual.setOnClickListener(this@MainActivity)
+            tvMinus.setOnClickListener(this@MainActivity)
+            tvMultiply.setOnClickListener(this@MainActivity)
+            tvPlus.setOnClickListener(this@MainActivity)
+            ivDeleteLast.setOnClickListener(this@MainActivity)
+            tvPoint.setOnClickListener(this@MainActivity)
+            tvPercentage.setOnClickListener(this@MainActivity)
+            tvN1.setOnClickListener(this@MainActivity)
+            tvN2.setOnClickListener(this@MainActivity)
+            tvN3.setOnClickListener(this@MainActivity)
+            tvN4.setOnClickListener(this@MainActivity)
+            tvN5.setOnClickListener(this@MainActivity)
+            tvN6.setOnClickListener(this@MainActivity)
+            tvN7.setOnClickListener(this@MainActivity)
+            tvN8.setOnClickListener(this@MainActivity)
+            tvN9.setOnClickListener(this@MainActivity)
+            tvN0.setOnClickListener(this@MainActivity)
+        }
+    }
+
+    private fun showResult(equation: String, result: String, operation: String) {
+
+        var res = ""
+        if (operation != "=") {
+            viewModel.setResult(result)
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.result.collect() {
+                    res = it
+                    println("resultFromVM : " + it)
+                }
+            }
+        }
+
+        with(binding) {
+            tvOperation.setText(equation)
+            if (res.endsWith(".0")) {
+                tvResult.text = res.dropLast(2)
+            } else {
+                tvResult.text = res
+            }
+        }
     }
 
     private fun themeSwitchView() {
@@ -68,6 +145,116 @@ class MainActivity : AppCompatActivity() {
                         R.color.activeThemeBtn
                     )!!.defaultColor
                 )
+            }
+        }
+    }
+
+    override fun onClick(clickedView: View?) {
+        var equation = binding.tvOperation.text.toString()
+        var result = binding.tvResult.text.toString()
+
+        with(binding) {
+            when (clickedView) {
+                tvAC -> {
+                    equation = ""
+                    result = ""
+                    operation = ""
+                    showResult(equation, result, operation)
+                }
+                ivDeleteLast -> {
+                    if (result.isNotEmpty()) {
+                        showResult(equation.dropLast(1), result.dropLast(1), operation)
+                    } else {
+
+                    }
+                }
+                tvDivision -> {
+                    if (operation.isEmpty())
+                        n1 = result.toDouble()
+                    operation = "÷"
+                    result = ""
+                    if (equation.isNotEmpty() && equation.last().toString() != " ") {
+                        equation += " ÷ "
+                    }
+                    showResult(equation, result, operation)
+                }
+                tvMinus -> {
+                    if (operation.isEmpty())
+                        n1 = result.toDouble()
+                    operation = "-"
+                    result = ""
+                    if (equation.isNotEmpty()) {
+                        equation += " - "
+                    }
+                    showResult(equation, result, operation)
+                }
+                tvPlus -> {
+                    if (result.isNotEmpty()){
+                        n1 += tvResult.text.toString().toDouble()
+                        operation = "+"
+
+                        n2 = tvResult.text.toString().toDouble()
+                        viewModel.add(n1, n2)
+                        println("operation Plus: " + tvResult.text.toString().toDouble().toString())
+                        if (equation.isNotEmpty()) {
+                            equation += " + "
+                            result = ""
+                        }
+                        showResult(equation, result, operation)
+                    } else {
+
+                    }
+                }
+                tvPercentage -> {
+                    Log.d("operaciaArChans", operation)
+                }
+                tvEqual -> {
+                    if (result.isNotEmpty()) {
+                        n2 = tvResult.text.toString().toDouble()
+                        when (operation) {
+                            "+" -> {
+                                operation = "="
+                                viewModel.add(n1, n2)
+                                println("n1 : " + n1)
+                                println("n2 : " + n2)
+                                showResult(equation, result, operation)
+                            }
+                            else -> {}
+                        }
+                    } else {
+
+                    }
+                }
+                tvPoint -> {
+                    if (equation.isNotEmpty()
+                        && result.isNotEmpty()
+//                        && !equation.contains(".")
+                        && !result.contains(".")
+                    ) {
+                        equation += "."
+                        result += "."
+                        showResult(equation, result, operation)
+                    } else {
+                    }
+                }
+                tvN0 -> {
+                    if (result != "0") {
+                        result += "0"
+                        equation += "0"
+                        showResult(equation, result, operation)
+                    } else {
+                    }
+                }
+                tvN1, tvN2, tvN3, tvN4, tvN5, tvN6, tvN7, tvN8, tvN9 -> {
+                    if (clickedView is TextView) {
+                        result += clickedView.text.toString()
+                        equation += clickedView.text.toString()
+                        println("operationOnNumber" + operation)
+                        showResult(equation, result, operation)
+                    } else {
+                    }
+                }
+                else -> {}
             }
         }
     }
--- app/src/main/java/com/example/mycalculatorapp/MainViewModel.kt
+++ app/src/main/java/com/example/mycalculatorapp/MainViewModel.kt
@@ -1,11 +1,22 @@
 package com.example.mycalculatorapp
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel : ViewModel() {
 
-    fun add(n1: Double, n2: Double): Double {
-        return (n1 + n2)
+    private val _result = MutableStateFlow<String>("")
+    val result = _result.asStateFlow()
+
+    fun setResult(result: String){
+        _result.value = result
+    }
+
+    fun add(n1: Double, n2: Double) {
+        _result.value = (n1 + n2).toString()
+        println("valueInAddFunc1 : " + n1.toString())
+        println("valueInAddFunc2 : " + n2.toString())
     }
 
     fun minus(n1: Double, n2: Double): Double {
